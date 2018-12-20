765b935d5fb6e6f00f389436b45fb15c8d2c12d6
Anthony Aylward <aaylward@eng.ucsd.edu>
2018-12-20 15:16:11 -0800

    build

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='luciferase',
     version='1.0',
+    author='Anthony Aylward',
+    author_email='aaylward@eng.ucsd.edu',
     description='Helper functions for luciferase data',
-    url=
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/anthony-aylward/luciferase.git',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
