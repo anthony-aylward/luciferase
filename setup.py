355e4b22fc057d41d44c1b436d1ade63787d103e
anthony-aylward <aaylward@eng.ucsd.edu>
2018-12-21 14:51:54 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='luciferase',
-    version='0.0.1',
+    version='0.0.2',
     author='Anthony Aylward',
     author_email='aaylward@eng.ucsd.edu',
     description='Helper functions for luciferase data',
