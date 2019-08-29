8b5468fa41e4ff1374bd17eb8cdcf57b156e7e71
anthony-aylward <aaylward@eng.ucsd.edu>
2019-08-28 20:04:44 -0700

    ylab

--- luciferase/ratioplot.py
+++ luciferase/ratioplot.py
@@ -180,7 +180,7 @@ def luciferase_ratioplot(
     sns.despine(trim=True, offset=10)
     ax1.tick_params(axis='both', length=6, width=1.25, bottom=True, left=True)
     ax1.set_xticklabels(xlab, rotation=45, ha='right')
-    ax1.set_ylabel('Ref:Alt ratio', fontsize=20)
+    ax1.set_ylabel(ylab, fontsize=20)
     ax1.set_title(title, fontsize=24, y=1.1)
 
     plt.savefig(output_file_path, bbox_inches='tight')
