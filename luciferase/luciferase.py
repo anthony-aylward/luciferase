6841d975a1a7a392561671f085f4e2401d6fbe4f
Anthony Aylward <aaylward@eng.ucsd.edu>
2020-02-03 17:21:44 -0800

    update help text

--- luciferase/luciferase.py
+++ luciferase/luciferase.py
@@ -48,9 +48,7 @@ JSON_EXAMPLES = """Examples of luciferase reporter data in JSON format:
   "Empty, ALPHA-TC6": [1.042, 0.92, 1.042]
 }
 
-The input JSON should contain either five entries or six entries. If it contains
-five entries, the bars of the resulting plot will have a 2-2-1 style. If it
-contains six entries, the bars will have a 2-1-2-1 style.
+.tsv, .csv, .xls, and .xlsx files are also accepted
 
 Significance indicators will be written above the bars: `***` if p<0.001,
 `**` if p<0.01, `*` if p<0.05, `ns` otherwise.
